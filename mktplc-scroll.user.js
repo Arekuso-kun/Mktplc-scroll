78dae898405ac3693533eab8b32e81254efee535
Bîrsan Dorin-Alexandru <83676537+Arekuso-kun@users.noreply.github.com>
2023-05-08 10:11:59 +0300

    I'm dumb

--- mktplc-scroll.user.js
+++ mktplc-scroll.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Alexo
-// @match        https://marketplace.tf
+// @match        https://marketplace.tf/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=marketplace.tf
 // @grant        none
 // ==/UserScript==
